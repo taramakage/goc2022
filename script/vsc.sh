cc52ce7beadc9c6f51db537ad99acd0a3f6d048a
yuandu <taramaoriskykage@gmail.com>
2022-11-28 15:32:50 +0800

    fix vsc count script

--- script/vsc.sh
+++ script/vsc.sh
@@ -1,33 +1,141 @@
 #!/bin/bash
 # This script counts on valid vsc message relayed by each address on Sputnik consumer chain.
+set -e 
 
-BINARY='./sputnik'
-JQ='jq'
+# Gloval Vars
+BINARY='./bin/sputnik'
 RPC='https://sputnik-rpc.ztake.org:443'
+CHAINID='sputnik'
+
+JQ='jq'
 EVENTS='ccv_packet.module=ccvconsumer'
-LIMIT=30
-VSC='vsc-output.txt'
-ANALYSIS='vsc-analysis.txt'
+LIMIT=10
+DATADIR='./vscdata'
+VSCSUFFIX='.vscfile.output'
+ANALYSIS='analysis.output'
+STARTPAGE="1"
+ENDPAGE="1"
 
-# Remove Output
-rm ${VSC} ${ANALYSIS}
+# Make directory for vsc data
+vsc-mkdir() {
+    if [ ! -d "${DATADIR}" ]; then
+        echo "Making directory: ${DATADIR}"
+        mkdir -p ${DATADIR}/${CHAINID}
+    else
+        echo "Directory already exists."
+    fi
+}
 
-# Get PageNumber
-echo "Fetching total page number..."
-PAGE=$(${BINARY} query txs --events ${EVENTS} --output json --limit 1 --node ${RPC} | ${JQ} '."page_total"')
-echo "--> Total page number is ${PAGE}."
+# Get Lateset Page Number
+vsc-fetch-latest-page() {
+    echo "Fetching newest total page number with page limit ${LIMIT}..."
+    ENDPAGE=$(${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --node ${RPC} | ${JQ} --raw-output '."page_total"')
+    echo "--> Total page number is ${ENDPAGE}."
+}
 
-# Query Loop  
-PAGE=1
-echo "Fetching txs..."
-for ((i=1;i<=PAGE;i++)); do 
-    echo "--> Fetching page ${i} on ${RPC} at $(date +"%T")"
-    ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${i} --node ${RPC} | ${JQ} --raw-output '.txs[] | "Signer " + (.tx.body.messages[] | select(."@type"=="/ibc.core.channel.v1.MsgRecvPacket" and .packet."source_port"=="provider")).signer + " Height " + .height + " TxHash " + .txhash' >> ${VSC}
-done
-echo "Finish fetching."
+# Generate Analysis File
+vsc-generate-analysis() {
+    # Check there's no empty file
+    echo "Checking fetched data..."
+    for file in ${DATADIR}/${CHAINID}/*${VSCSUFFIX}; do
+        if [[ -z $(grep '[^[:space:]]' $file) ]]; then
+            echo "--> $file is empty, unable to continue..."
+            exit 1
+        fi
+    done
+    echo "Passing check."
 
-# Sort by signer then height
-echo "Generating analysis file ${ANALYSIS}..."
-sort -k2,2 -k4,4n < ${VSC} | cut -d ' ' -f 2 | uniq -c | sort -k1,1n | awk '{print $2,$1}' >> ${ANALYSIS}                  
-sed -i '1s/^/Signer                                        Times\n/' ${ANALYSIS}
-echo "--> Done."
+    ANLSTMP=${DATADIR}/${CHAINID}/${ANALYSIS}.tmp
+    echo "Generating analysis file ${ANALYSIS}..."
+    touch ${ANLSTMP}
+    for file in ${DATADIR}/${CHAINID}/*${VSCSUFFIX}; do
+        cat ${file} >> ${ANLSTMP}
+    done
+    sort -k2,2 -k4,4n < ${ANLSTMP} | cut -d ' ' -f 2 | uniq -c | sort -k1,1n | awk '{print $2,$1}' > ${DATADIR}/${CHAINID}/${ANALYSIS}
+    sed -i '1s/^/Signer                                        Times\n/' ${DATADIR}/${CHAINID}/${ANALYSIS}
+    rm ${ANLSTMP}
+    echo "--> Done."    
+}
+
+# Retry fetch events
+vsc-retry-fetch() {
+    for file in ${DATADIR}/${CHAINID}/*${VSCSUFFIX}; do
+        if [[ -z $(grep '[^[:space:]]' $file) ]]; then
+            echo "--> $file is empty..."
+            number=$(echo $file | sed -e s/[^0-9]//g)
+            ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${number} --node ${RPC} | ${JQ} --raw-output '.txs[] | "Signer " + (.tx.body.messages[] | select(."@type"=="/ibc.core.channel.v1.MsgRecvPacket" and .packet."source_port"=="provider")).signer + " Height " + .height + " TxHash " + .txhash' > ${file}
+        fi
+    done
+}
+
+# Fetch single page
+vsc-fetch-single() {
+    vsc-mkdir
+
+    if [ "$#" -ne 1 ]; then
+        echo "Page arg not speicified"
+        exit 1
+    fi
+
+    VSCFILE=${DATADIR}/${CHAINID}/${1}${VSCSUFFIX}
+    ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${1} --node ${RPC} | ${JQ} --raw-output '.txs[] | "Signer " + (.tx.body.messages[] | select(."@type"=="/ibc.core.channel.v1.MsgRecvPacket" and .packet."source_port"=="provider")).signer + " Height " + .height + " TxHash " + .txhash' > ${VSCFILE}
+}
+
+vsc-fetch-all() {
+    vsc-mkdir
+
+    vsc-fetch-latest-page
+
+    # Get Last Fechted Page Number
+    STARTPAGE=$(ls -av ${DATADIR}/${CHAINID} | grep ${VSCSUFFIX} | tail -1 | sed -e s/[^0-9]//g )
+    if [ -z "${STARTPAGE}" ]; then
+        STARTPAGE="1"
+    fi
+    echo "Last Fetching page number is ${STARTPAGE}"
+
+    # Query Loop 
+    echo "Fetching txs..."
+    for (( i=${STARTPAGE}; i<=${ENDPAGE}; i++ )); do 
+        echo "--> Fetching page ${i} on ${RPC} at $(date +"%T")"
+        VSCFILE=${DATADIR}/{CHAINID}/${i}${VSCSUFFIX}
+        ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${i} --node ${RPC} | ${JQ} --raw-output '.txs[] | "Signer " + (.tx.body.messages[] | select(."@type"=="/ibc.core.channel.v1.MsgRecvPacket" and .packet."source_port"=="provider")).signer + " Height " + .height + " TxHash " + .txhash' > ${VSCFILE}
+    done
+    echo "Finish fetching."
+
+    # Retry at most three times
+    for (( i=1; i<=3; i++ )) do 
+        echo "--> retry $i times"
+        vsc-retry-fetch
+    done
+
+    # Generate Analysis
+    vsc-generate-analysis
+}
+
+# Test Function
+checkpage() {
+    vsc-mkdir
+
+    echo "Fetching page ${i} twice with page limit ${LIMIT}..."
+    echo "--> Fetching for the first time..."
+    ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${1} --node ${RPC} | ${JQ} --raw-output . > ${DATADIR}/test-1
+    sleep 1
+    echo "--> Fetching for the second time..."
+    ${BINARY} query txs --events ${EVENTS} --output json --limit ${LIMIT} --page ${1} --node ${RPC} | ${JQ} --raw-output . > ${DATADIR}/test-2
+    if cmp -s ${DATADIR}/test-1 ${DATADIR}/test-2
+    then
+        echo "--> The files match."
+    else
+        echo "--> WARN: The files are different."
+    fi
+    rm ${DATADIR}/test-1 ${DATADIR}/test-2
+}
+
+# Script Entrance
+if declare -f "$1" > /dev/null
+then
+    "$@"
+else    
+    echo "$1 is not a known function name" >& 2
+    exit 1
+fi
